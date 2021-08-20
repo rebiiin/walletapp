63e0f3371629caaa623cd3133e0fecd3a19cfdee
Rebin <4rebin@gmail.com>
2021-08-20 16:59:58 +0300

    Add all files into Repo

---
+++ src/context/GlobalState.js
@@ -0,0 +1,57 @@
+import React, {createContext,useReducer} from 'react';
+import AppReducer  from './AppReducer';
+//initial state
+const initailState = {
+
+        transactions: []
+
+
+            // just ia hve commented this section
+            // transactions: [
+            //   { id: 1, text: 'Flower', amount: -20 },
+            //   { id: 2, text: 'Salary', amount: 300 },
+            //   { id: 3, text: 'Book', amount: -10 },
+            //   { id: 4, text: 'Camera', amount: 150 }
+            // ]
+
+}
+//Create contaxt
+ export const GlobalContext  = createContext(initailState);
+
+
+ // Provider component
+
+ export const GlobalProvider = ({ children }) => {
+    const [state , dispatch] = useReducer(AppReducer, initailState);
+
+    //Actions
+
+   function deleteTransaction(id) {
+       dispatch({
+          type: 'DELETE_TRANSACTION',
+          payload: id
+        
+       });
+   }
+  
+
+   function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions ,
+        deleteTransaction,
+        addTransaction
+    }}>
+       
+        {children}
+
+    </GlobalContext.Provider>)
+ }
+
+
